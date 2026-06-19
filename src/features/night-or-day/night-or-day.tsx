0c7a085349e12a5ba342464b80ef4da2c3242fae
Melina Rcldz <ricaldezmelina123@gmail.com>
2026-06-19 14:09:10 -0300

    fix: enable class-based dark mode

--- src/features/night-or-day/night-or-day.tsx
+++ src/features/night-or-day/night-or-day.tsx
@@ -101,7 +101,9 @@ export function ThemeToggle() {
     const root = window.document.documentElement;
     if (theme === 'light') {
       root.classList.add('light');
+      root.classList.remove('dark');
     } else {
+      root.classList.add('dark');
       root.classList.remove('light');
     }
     localStorage.setItem('theme', theme);
